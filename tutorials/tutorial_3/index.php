b63a5c2984de3ab75398eb0805a477ac70445978
wintshwesin1 <wintshwesin211@gmail.com>
2022-02-19 13:52:03 +0630

    [master] fix - calculate age in php instead of javascript

--- tutorials/tutorial_3/index.php
+++ tutorials/tutorial_3/index.php
@@ -1,3 +1,11 @@
+<?php 
+    if(isset($_POST['submit'])) {
+        $birthDate = $_POST['date-of-birth'];
+        $currentDate = date("d-m-Y");
+        $age = date_diff(date_create($birthDate), date_create($currentDate))->format("%y");
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,30 +13,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tutorial 3</title>
-
-    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="http://code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
-    <link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    
+    <script src="./js/jquery-1.9.1.js"></script>
+    <script src="./js/bootstrap-datepicker.min.js"></script>
+    <link rel="stylesheet" href="./css/bootstrap-datepicker3.css"/>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/jquery-ui.js"></script>
+    <link rel="stylesheet" href="./css/jquery-ui.css">
     
     <style>
-        .container{
-            text-align: center;
-            width: 250px;
-            margin: 10px auto;
-            color:#333;
+        .container {
+            text-align : center;
+            width : 250px;
+            margin : 10px auto;
+            color :#333;
         }
 
-        h3{
-            font-size: 40px;
+        h3 {
+            font-size : 40px;
         }
 
-        .form-input{
-            width:100%;
-            margin-bottom:20px;
-            padding:5px 15px;
+        .form-input {
+            width : 100%;
+            margin-top : 10px;
+            margin-bottom : 20px;
+            padding : 5px 15px;
+            border: 1px solid #00000090;
+            border-radius:5px;
+        }
+
+        .calculatebtn {
+            margin-bottom : 20px;
+            width: 80px;
+            padding: 8px;
+            background-color: #1CB6E0;
+            border: 1px solid #1CB6E0;
+            border-radius:5px;
+            color: #fff;
+        }
+
+        .showresult {
+            font-size : 20px;
+            color : #333;
+            font-weight : bold ;
         }
 
     </style>
@@ -37,28 +64,29 @@
     <div class="container">
       <h3>Calculate Age</h3>
       
-        <label for="date-of-birth">Date Of Birth:</label>
-        <input class="date form-input" type="text" name="date-of-birth" autocomplete="off">
-        <label for="age">Age:</label>
-        <input type="text" id="age" class="form-input">
+        <form action="." method="POST">
+            <label for="date-of-birth">Date Of Birth:</label>
+            <input class="date form-input" type="text" name="date-of-birth" autocomplete="off" required>
+            <input type="submit" name="submit" value="Calculate" class="calculatebtn">
+        </form>
+        <div class="showresult"><?php if(!empty($age)){ echo "Age is : " .$age."years"; } ?></div>
         
     </div>
 
     
     <script>
-      $(document).ready(function() {
-        var age = "";
-        $('.date').datepicker({
-            onSelect: function (value, ui) {
-                    var today = new Date();
-                        age = today.getFullYear() - ui.selectedYear;
-                    $('#age').val(age); 
-            },
-            changeMonth: true,
-            changeYear: true,
-            yearRange: '1850:2050',
+        $(document).ready(function() {
+            $('.date').datepicker({
+                dateFormat: 'yy-mm-dd',
+                changeMonth: true,
+                changeYear: true,
+                yearRange: '1850:2050',
+            });
+
+            if ( window.history.replaceState ) {
+                window.history.replaceState( null, null, window.location.href );
+            }
         });
-      });
     </script>
   
 </body>
