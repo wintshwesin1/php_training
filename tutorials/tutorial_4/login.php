c85284c39c7c3565eaff8eca03d46d2215a3b811
wintshwesin1 <wintshwesin211@gmail.com>
2022-02-22 08:36:11 +0630

    [master] fix - check the sesssion exists

--- tutorials/tutorial_4/login.php
+++ tutorials/tutorial_4/login.php
@@ -12,7 +12,7 @@
 <body>
 
 <?php
-    if($_SESSION["username"]) {
+    if(isset($_SESSION["username"])) {
 ?>
     Welcome <?php echo $_SESSION["username"]; ?>. Click here to <a href="logout.php" tite="Logout">Logout.
 <?php
