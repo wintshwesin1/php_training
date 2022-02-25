abf4f0963ac29ddb89ef898e3c78ce26aba0df74
wintshwesin1 <wintshwesin211@gmail.com>
2022-02-25 15:58:01 +0630

    [master] added files for laravel testing

---
+++ laravel/routes/web.php
@@ -0,0 +1,55 @@
+<?php
+use App\Models\Task;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', function () {
+    return view('tasks');
+});
+
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+ 
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+ 
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+ 
+    return redirect('/');
+});
+
+Route::get('/', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+ 
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+ 
+    return redirect('/');
+});
